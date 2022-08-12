8568d7a71563d8905e59c7f217b3eae246c7dcaf
Izabela Guimarães <106126259+Belorzye@users.noreply.github.com>
2022-08-12 20:57:07 -0300

    Add files via upload

---
+++ mesa-04.js
@@ -0,0 +1,9 @@
+var nome = 'Izabela';
+var sobrenome = 'Guimarães';
+var idade = 20;
+var peso = 48.7;
+var altura = 1,61;
+var plano = true;
+var imc = peso / Math.pow(altura,2);
+
+console .log (nome + "" + sobrenome + "Tem" + idade + "anos e seu indice de massa corporal é de" + imc);
