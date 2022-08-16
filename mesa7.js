72f703a561dd0b0de98e437387042d5f1b411b6a
Izabela Guimarães <106126259+Belorzye@users.noreply.github.com>
2022-08-15 22:41:59 -0300

    Add files via upload

---
+++ mesa7.js
@@ -0,0 +1,18 @@
+// EXERCÍCIO 1 
+
+// 1- False
+// 2- True
+// 3- True
+// 4- True
+// 5- False
+// 6- True
+// 7- True
+// 8- False
+// 9- False
+// 10- a) False; b) True; c) True;
+//
+// EXERCÍCIO 2
+
+// 1- True
+// 2- False
+// 3- False
