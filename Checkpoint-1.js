80fccf18335d6c8fc1634dec0119d51f47b04280
Izabela Guimarães <106126259+Belorzye@users.noreply.github.com>
2022-08-17 22:44:25 -0300

    Add files via upload

---
+++ Checkpoint-1.js
@@ -0,0 +1,41 @@
+const pipoca = 10;
+const macarrao = 8;
+const carne = 15;
+const feijao = 12;
+const brigadeiro = 8;
+
+
+function microondas (opcao, tempo) {
+
+ {
+
+    return "Prato inexistente";
+}
+
+if (tempo <= tempoPadrao ){
+
+    return "Tempo insuficiente";
+}
+
+else if (tempo = tempoPadrao) {
+
+    return "Prato pronto, bom apetite!"
+}
+
+else if (tempo >= (2 * tempoPadrao) && tempo <= (3 * tempoPadrao)) {
+    
+    return "A comida queimou!";
+}
+
+else if (tempo > 3* tempoPadrao) {
+    
+    return "Kabummm!"
+}
+
+
+}
+
+console .log (microondas ("3 - carne", 30) + " Prato pronto, bom apetite!")
+
+
+
