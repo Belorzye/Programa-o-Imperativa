abf394705d6bea31d0c22c7be91d63d871ff2e9e
Izabela Guimarães <106126259+Belorzye@users.noreply.github.com>
2022-08-24 22:47:11 -0300

    Add files via upload

---
+++ mesa12.js
@@ -0,0 +1,40 @@
+const alicia = [23, 69, 32];
+const bob = [12, 67, 43];
+
+function encontrarGanhador (a, b) {
+    let pontosPrimeiroParticipante = 0;
+    let pontosSegundoParticipante = 0;
+
+    for(let i = 0; i<a.length;i++){
+        if(a[i] > b[i]){
+            pontosPrimeiroParticipante = pontosPrimeiroParticipante + 1;
+        }
+        else if(a[i] < b[i]){
+            pontosSegundoParticipante = pontosSegundoParticipante + 1;
+        }
+        else{
+            pontosPrimeiroParticipante = pontosPrimeiroParticipante + 0;
+            pontosSegundoParticipante = pontosSegundoParticipante + 0;
+        }
+
+    }
+
+    let resultado
+
+    if(pontosPrimeiroParticipante > pontosSegundoParticipante){
+         resultado = 'Alicia'
+    }
+
+    else if(pontosPrimeiroParticipante < pontosSegundoParticipante){
+        resultado = 'Bob'
+    }
+    
+    else{
+        resultado = 'Empatado!'
+    }
+
+    return resultado
+
+}
+
+console. log('O ganhador é: ' + encontrarGanhador (alicia, bob)); 
