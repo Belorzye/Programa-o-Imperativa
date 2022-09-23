02a45eeffd0cf8b6f1bd729d8397fe1a225a6e86
Izabela Guimarães <106126259+Belorzye@users.noreply.github.com>
2022-09-23 20:17:48 -0300

    Add files via upload

---
+++ Checkpoint3/estudantes.js
@@ -0,0 +1,54 @@
+const listaDeAlunos = [
+    {
+        nome: "Izabela",
+        faltas: 3,
+        notas: [10, 8, 9.5, 10]
+    },
+    {
+        nome: "Leo",
+        faltas: 3,
+        notas: [6, 7, 5, 5]
+    },
+    {
+        nome: "Larissa",
+        faltas: 2,
+        notas: [6, 7.3, 5, 8]
+    },
+    {
+        nome: "Vinicius",
+        faltas: 1,
+        notas: [7, 10, 3, 10]
+    },
+    {
+        nome: "Nathy",
+        faltas: 6,
+        notas: [7, 7, 10, 9]
+    },
+    {
+        nome: "Vick",
+        faltas: 0,
+        notas: [8, 10, 5, 5]
+    },
+    {
+        nome: "Drica",
+        faltas: 0,
+        notas: [10, 8, 9, 10]
+    },
+    {
+        nome: "Caio",
+        faltas: 1,
+        notas: [10, 10, 10, 9.5]
+    },
+    {
+        nome: "Bruno",
+        faltas: 2,
+        notas: [5, 6, 5, 5]
+    },
+    {
+        nome: "Fabiana",
+        faltas: 3,
+        notas: [10, 10, 10, 10]
+    }
+]
+
+module.exports = listaDeAlunos;
